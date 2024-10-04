1bb6a1f302f343b31112e8dc64a0bb8e9be8df17
Christian Shum-Harden <christian.shumharden@shopify.com>
2024-10-03 17:05:33 -0700

    fix error

--- tap_sftp/client.py
+++ tap_sftp/client.py
@@ -17,11 +17,13 @@ LOGGER = singer.get_logger()
 
 def handle_backoff(details):
     LOGGER.warn(
-        "SSH Connection closed unexpectedly. Waiting {wait} seconds and retrying...".format(**details)
+        "SSH Connection closed unexpectedly. Waiting {wait} seconds and retrying...".format(
+            **details
+        )
     )
 
 
-class SFTPConnection():
+class SFTPConnection:
     def __init__(self, host, username, password=None, private_key_file=None, port=None):
         self.host = host
         self.username = username
@@ -45,35 +47,42 @@ class SFTPConnection():
         max_tries=7,
         on_backoff=handle_backoff,
         jitter=None,
-        factor=2)
+        factor=2,
+    )
     def __connect(self):
-        LOGGER.info('Creating new connection to SFTP...')
+        LOGGER.info("Creating new connection to SFTP...")
         self._attempt_connection()
-        LOGGER.info('Connection successful')
+        LOGGER.info("Connection successful")
 
     def _attempt_connection(self):
-        client = paramiko.SSHClient()
-        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.get_transport().get_security_options().key_types = ['ssh-rsa', 'ecdsa-sha2-nistp256']
+        transport = paramiko.Transport(
+            hostname=self.host,
+            port=self.port,
+        )
+        transport.get_security_options().ciphers = ("ssh-rsa", "ecdsa-sha2-nistp256")
         try:
-            client.connect(
-                hostname=self.host,
-                port=self.port,
+            transport.connect(
                 username=self.username,
                 password=self.password,
                 pkey=self.key,
                 compress=True,
-                timeout=120
+                timeout=120,
             )
+            client = paramiko.SSHClient().from_transport(transport)
+            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             self.sftp = client.open_sftp()
         except paramiko.AuthenticationException as ex:
-            LOGGER.error('AuthenticationException, please verify your credentials: %s', ex)
+            LOGGER.error(
+                "AuthenticationException, please verify your credentials: %s", ex
+            )
         except paramiko.SSHException as ex:
-            LOGGER.error('SSHException, could not establish SSH connection: %s', ex)
+            LOGGER.error("SSHException, could not establish SSH connection: %s", ex)
         except paramiko.BadHostKeyException as ex:
-            LOGGER.error('BadHostKeyException, bad host key: %s', ex)
+            LOGGER.error("BadHostKeyException, bad host key: %s", ex)
         except Exception as ex:
-            LOGGER.error('Exception, failed to connect or establish SFTP session: %s', ex)
+            LOGGER.error(
+                "Exception, failed to connect or establish SFTP session: %s", ex
+            )
         finally:
             if client:
                 client.close()
@@ -86,7 +95,11 @@ class SFTPConnection():
             self.decrypted_file.close()
 
     def match_files_for_table(self, files, table_name, search_pattern):
-        LOGGER.info("Searching for files for table '%s', matching pattern: %s", table_name, table_pattern)
+        LOGGER.info(
+            "Searching for files for table '%s', matching pattern: %s",
+            table_name,
+            table_pattern,
+        )
         matcher = re.compile(search_pattern)
         return [f for f in files if matcher.search(f["filepath"])]
 
@@ -104,8 +117,8 @@ class SFTPConnection():
         """
         files = []
 
-        if prefix is None or prefix == '':
-            prefix = '.'
+        if prefix is None or prefix == "":
+            prefix = "."
 
         try:
             result = self.sftp.listdir_attr(prefix)
@@ -115,21 +128,29 @@ class SFTPConnection():
         for file_attr in result:
             # NB: This only looks at the immediate level beneath the prefix directory
             if self.is_directory(file_attr):
-                files += self.get_files_by_prefix(prefix + '/' + file_attr.filename)
+                files += self.get_files_by_prefix(prefix + "/" + file_attr.filename)
             else:
                 if self.is_empty(file_attr):
                     continue
 
                 last_modified = file_attr.st_mtime
                 if last_modified is None:
-                    LOGGER.warning("Cannot read m_time for file %s, defaulting to current epoch time",
-                                   os.path.join(prefix, file_attr.filename))
+                    LOGGER.warning(
+                        "Cannot read m_time for file %s, defaulting to current epoch time",
+                        os.path.join(prefix, file_attr.filename),
+                    )
                     last_modified = datetime.utcnow().timestamp()
 
                 # NB: SFTP specifies path characters to be '/'
                 #     https://tools.ietf.org/html/draft-ietf-secsh-filexfer-13#section-6
-                files.append({"filepath": prefix + '/' + file_attr.filename,
-                              "last_modified": datetime.utcfromtimestamp(last_modified).replace(tzinfo=pytz.UTC)})
+                files.append(
+                    {
+                        "filepath": prefix + "/" + file_attr.filename,
+                        "last_modified": datetime.utcfromtimestamp(
+                            last_modified
+                        ).replace(tzinfo=pytz.UTC),
+                    }
+                )
 
         return files
 
@@ -143,57 +164,70 @@ class SFTPConnection():
         matching_files = self.get_files_matching_pattern(files, search_pattern)
 
         if matching_files:
-            LOGGER.info('Found %s files in "%s" matching "%s"', len(matching_files), prefix, search_pattern)
+            LOGGER.info(
+                'Found %s files in "%s" matching "%s"',
+                len(matching_files),
+                prefix,
+                search_pattern,
+            )
         else:
-            LOGGER.warning('Found no files on specified SFTP server at "%s" matching "%s"', prefix, search_pattern)
-    
+            LOGGER.warning(
+                'Found no files on specified SFTP server at "%s" matching "%s"',
+                prefix,
+                search_pattern,
+            )
+
         if modified_since is not None:
             LOGGER.info("Processing files modified since: %s", modified_since)
-            matching_files = [f for f in matching_files if f["last_modified"] > modified_since]
+            matching_files = [
+                f for f in matching_files if f["last_modified"] > modified_since
+            ]
 
         for f in matching_files:
-            LOGGER.info("Found file: %s", f['filepath'])
-        
+            LOGGER.info("Found file: %s", f["filepath"])
+
         matching_files = sorted(matching_files, key=lambda x: x["last_modified"])
         return matching_files
 
     def get_file_handle(self, f, decryption_configs=None):
-        """ Takes a file dict {"filepath": "...", "last_modified": "..."} and returns a handle to the file. """
+        """Takes a file dict {"filepath": "...", "last_modified": "..."} and returns a handle to the file."""
         with tempfile.TemporaryDirectory() as tmpdirname:
             sftp_file_path = f["filepath"]
-            local_path = f'{tmpdirname}/{os.path.basename(sftp_file_path)}'
+            local_path = f"{tmpdirname}/{os.path.basename(sftp_file_path)}"
             if decryption_configs:
-                LOGGER.info(f'Decrypting file: {sftp_file_path}')
+                LOGGER.info(f"Decrypting file: {sftp_file_path}")
                 # Getting sftp file to local, then reading it is much faster than reading it directly from the SFTP
                 self.sftp.get(sftp_file_path, local_path)
                 decrypted_path = decrypt.gpg_decrypt(
                     local_path,
                     tmpdirname,
-                    decryption_configs.get('key'),
-                    decryption_configs.get('gnupghome'),
-                    decryption_configs.get('passphrase')
+                    decryption_configs.get("key"),
+                    decryption_configs.get("gnupghome"),
+                    decryption_configs.get("passphrase"),
                 )
-                LOGGER.info(f'Decrypting file complete')
+                LOGGER.info(f"Decrypting file complete")
                 try:
-                    self.decrypted_file = open(decrypted_path, 'rb')
+                    self.decrypted_file = open(decrypted_path, "rb")
                 except FileNotFoundError:
-                    raise Exception(f'Decryption of file failed: {sftp_file_path}')
+                    raise Exception(f"Decryption of file failed: {sftp_file_path}")
                 return self.decrypted_file, decrypted_path
             else:
                 self.sftp.get(sftp_file_path, local_path)
-                return open(local_path, 'rb')
+                return open(local_path, "rb")
 
     def get_files_matching_pattern(self, files, pattern):
-        """ Takes a file dict {"filepath": "...", "last_modified": "..."} and a regex pattern string, and returns
-            files matching that pattern. """
+        """Takes a file dict {"filepath": "...", "last_modified": "..."} and a regex pattern string, and returns
+        files matching that pattern."""
         matcher = re.compile(pattern)
         LOGGER.info(f"Searching for files for matching pattern: {pattern}")
         return [f for f in files if matcher.search(f["filepath"])]
 
 
 def connection(config):
-    return SFTPConnection(config['host'],
-                          config['username'],
-                          password=config.get('password'),
-                          private_key_file=config.get('private_key_file'),
-                          port=config.get('port'))
+    return SFTPConnection(
+        config["host"],
+        config["username"],
+        password=config.get("password"),
+        private_key_file=config.get("private_key_file"),
+        port=config.get("port"),
+    )
